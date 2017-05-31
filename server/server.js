d470624f38cd93431ef25bf9aba1c09711332789
Zdenek Skrobak <z.skrobak@hotmail.cz>
2017-05-31 08:35:40 +0200

    First commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+// console.log(__dirname + '/../public');
+// console.log(publicPath);
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}.`);
+});
